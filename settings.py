afa2eb68acd93e4eaf5f352bd52ebe0d2bbd46b6
Vitaliy <mr.vitaliy.konstantinov@mail.ru>
2021-12-04 15:56:28 +0600

    Init: missing psycopg2 dependency fixed #4

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 import os
 
 # DEV_DATABASE_URL = 'postgresql+psycopg2://postgres:postgres@localhost:5432/csci341_hw2_task2'
-
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = 'postgres://seiqfdyoztqimw:c1333343f6a57cb75009edd11c415850c0f79875dbfa5f248d4f7091e4d08371@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d2s3tv4urbng7p'
