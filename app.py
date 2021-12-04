7d7eb84efe27913e408e0c21d01d2243337e8a37
Vitaliy <mr.vitaliy.konstantinov@mail.ru>
2021-12-04 16:36:51 +0600

    Init: db init and init-db fill-mock-db commands to flask cli

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
 from settings import DATABASE_URL
 
+import click
+from flask import Flask
+from flask.cli import with_appcontext
+
+from pathlib import Path
 from sqlalchemy.engine.base import Engine
 from sqlalchemy import text, create_engine
-from pathlib import Path
-from flask import Flask
 
 
-def init_db(engine: Engine, filepath: Path):
+app = Flask(__name__)
+engine = create_engine(DATABASE_URL, echo=False)
+
+
+@click.command(name='init-db')
+@with_appcontext
+def init_db():
+	filepath = Path('db/init.sql')
+
 	with engine.begin() as conn:
 	    for query in [x.strip() for x in filepath.read_text().split(';') if x.strip()]:
 	        conn.execute(text(query))
 
 
-app = Flask(__name__)
-engine = create_engine(DATABASE_URL, echo=False)
-init_db(engine, Path('init.sql'))
+@click.command(name='fill-mock-db')
+@with_appcontext
+def fill_mock_db():
+	filepath = Path('db/mock.sql')
+
+	with engine.connect() as conn:
+	    for query in [x.strip() for x in filepath.read_text().split(';') if x.strip()]:
+	        conn.execute(text(query))
+
+
+app.cli.add_command(init_db)
+app.cli.add_command(fill_mock_db)
 
 
 @app.route('/', methods=['GET'])
 def homepage():
-	with engine.begin() as conn:
+	with engine.connect() as conn:
 		users = conn.execute("""
 			SELECT email FROM Users;
 		""").fetchall()
@@ -27,6 +47,3 @@ def homepage():
 	return {
 		'users': [user[0] for user in users]
 	}
-
-
-app.run(debug=True)
